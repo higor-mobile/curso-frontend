10cb23b4238838b87bedf9c745197bac2ff6d6bd
Higor Freitas <higor.mobile@gmail.com>
2021-11-30 03:12:55 -0300

    avançando nos módulos

---
+++ javascript/js/script.js
@@ -0,0 +1,65 @@
+
+function calcularMedia( notas ) {
+
+    let soma = 0;
+    for( c = 0; c < notas.length; c++) {
+        soma += notas[c];
+    }
+
+    media = soma / notas.length;
+    return media;
+}
+
+let media; //escopo global
+
+function aprovacao( notas ) {
+    let media = calcularMedia( notas ); //escopo da função
+
+    let condicao = media >= 8? "aprovado" : "reprovado";
+
+    return "Média: " + media + " - Resultado: " + condicao;
+}
+
+// Funções Recursivas
+
+function contagemRegressiva(numero){
+    console.log(numero);
+    let proximoNumero = numero - 1;
+    if(proximoNumero > 0)
+    contagemRegressiva(proximoNumero);
+        
+}
+
+//console.log( "Média: " + calcularMedia([10, 4, 6]))
+//console.log( aprovacao( calcularMedia([10, 4, 6])))
+
+
+document.addEventListener('submit', function( evento ){
+    evento.preventDefault();
+    evento.stopPropagation();
+
+    let formulario = document.getElementById('formulario-01');
+
+    let dados = new FormData(formulario);
+
+    let objeto = {};
+
+    let notas = [];
+
+    for(let key of dados.keys()) {
+        objeto[key] = dados.get(key);
+
+        //adiciona itens no array
+        notas.push( parseInt(dados.get(key)));
+    }
+
+    console.log(notas);
+
+    console.log(objeto);
+
+    texto = aprovacao(notas)
+
+    document.getElementById('resultado').innerHTML = texto;
+});
+
+
